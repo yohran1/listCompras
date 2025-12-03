b9da3b99262d08be295309aed404d1155aa6c6fe
Yohran Kallahari <yohranv@gmail.com>
2025-12-03 00:01:46 -0300

    Aplicando gatilhos e valores no front

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,10 @@ function App() {
   const [error, setError] = useState('')
   // Função para formatar  valor como moeda BRL
   const formatCurrency = (value) => {
-    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)
+    return new Intl.NumberFormat('pt-BR', { 
+      style: 'currency', 
+      currency: 'BRL'
+     }).format(value)
   }
   // Função chamada ao clicar no botão de adicionar item
   const handleAddItem = () => {
@@ -27,21 +30,25 @@ function App() {
       setError('Por favor, insira o nome do item.')
       return // Se houver um erro de validação, o return interrompe a execução da função imediatamente
     }
+    if(!price.trim()){
+      setError('Por favor, insira o valor do item.')
+      return
+    }
+    // Aceita vírgula ou ponto como separador decimal
+    const normalized = price.replace(',', '.')
+    // Converte o valor para número de ponto flutuante
+    const parsed = parseFloat(normalized)
+    if(isNaN(parsed)){
+      setError('Por favor, insira um valor numérico (ex: 12.50 ).')
+      return
+    }
+    // Criar novo item e atualizar lista
+    const newItem = { name, price: parsed}
+    setItems(prev => [...prev, newItem ])
+    // limpa campos
+    setItemName('')
+    setPrice('')
   }
-  // Aceita vírgula ou ponto como separador decimal
-  const normalized = price.replace(',')
-  // Converte o valor para número de ponto flutuante
-  const parsed = parseFloat(normalized)
-  if(isNaN(parsed)){
-    setError('Por favor, insira um valor numérico (ex: 12.50 ).')
-    return
-  }
-  // Criar novo item e atualizar lista
-  const newItem = { name, price: parsed}
-  setItems(prev => [...prev, newItem ])
-  // limpa campos
-  setItemName('')
-  setPrice('')
   // Permitir enviar com Enter no campo do preço
   const handleKeyDown = (e) => {
     if(e.key ==='Enter') handleAddItem()
@@ -57,16 +64,42 @@ function App() {
         <div className="boxAddItems">
           <h2>Adicionar item</h2>
           <div className="addItems">
-            <input type="text" name="item" placeholder='Nome do item'/>
-            <input type="number" name="valor" placeholder='Valor (R$)'/>
-            <button>+ Adicionar</button>
+            <input
+             type="text" 
+             name="item" 
+             placeholder='Nome do item'
+             value={itemName}
+             onChange={(e) => setItemName(e.target.value)}
+             />
+            <input 
+            type="number" 
+            name="valor" 
+            placeholder='Valor (R$)'
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            onKeyDown={handleKeyDown}
+            step="0.01"
+            />
+            <button onClick={handleAddItem}>+ Adicionar</button>
           </div>
-          
+          {error && <p className="error">{error}</p>}
         </div>
         <div className="itemsList">
           <div className="items">
-              <p>Nenhum item na lista</p>
-              <p>Adicione itens acima para começar</p>
+              {items.length === 0 ? (
+                <>
+                  <p>Nenhum item na lista</p>
+                  <p>Adicione itens acima para começar</p>
+                </>
+              ) : (
+                <ul>
+                  {items.map((item, id_item) => (
+                    <li key={id_item}>
+                      {item.name} - {formatCurrency(item.price)}
+                    </li>
+                  ))}
+                </ul>
+              )}
           </div>
         </div>
       </section>
